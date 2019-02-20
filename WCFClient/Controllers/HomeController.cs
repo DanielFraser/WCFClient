536c19e4bf56e3bb94ee9ec2d05b87295b5f093b
Daniel R Fraser <dukeydk@gmail.com>
2019-02-20 18:27:56 -0500

    Added views

--- WCFClient/Controllers/HomeController.cs
+++ WCFClient/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using WCFClient.EmployeeReference;
 
 namespace WCFClient.Controllers
 {
@@ -16,7 +16,7 @@ namespace WCFClient.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
@@ -25,6 +25,34 @@ namespace WCFClient.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Employees()
+        {
+            EmployeeServiceClient esc = new EmployeeServiceClient();
+            ViewBag.employees = esc.GetAll().ToList();
+            ViewBag.props = typeof(EmployeeEnt).GetProperties().Where(a => a.Name != "ExtensionData").ToList();
+            return View();
+        }
+
+        public void AddEmployee(string name, DateTime hiredate, decimal salary, string deptname, string address)
+        {
+            EmployeeServiceClient esc = new EmployeeServiceClient();
+            esc.AddEmployee(name, hiredate, salary, deptname, address);
+        }
+
+        public void DelEmployee(int id)
+        {
+            EmployeeServiceClient esc = new EmployeeServiceClient();
+            esc.DelEmployee(id);
+        }
+
+        public void EditEmployee(int id, string name, DateTime hiredate, decimal salary, string deptname, string address)
+        {
+            EmployeeServiceClient esc = new EmployeeServiceClient();
+            EmployeeEnt e = new EmployeeEnt();
+            e.GetType().GetProperties();
+            esc.EditEmployee(id, name, hiredate, salary, deptname, address);
         }
     }
 }
---
+++ WCFClient/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WCFClient.Models
+{
+    public class EmployeeModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime? Hiredate { get; set; }
+
+        [Required]
+        [Range(0.01, 999999999, ErrorMessage = "Salary must be greater than 0.00")]
+        public decimal? Salary { get; set; }
+
+        [Required]
+        public string Deptname { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
